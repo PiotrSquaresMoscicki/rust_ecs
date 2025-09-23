98e69f76f1fd90efbc7dceea6ec5c994b4a44915
Piotr Squares Mościcki <piotr.squares.moscicki@gmail.com>
2025-09-23 06:17:29 +0000

    Remove lib.backup and unnecessary export

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,16 +1,16 @@
 pub mod components;
-pub mod utils;
-pub mod navigation_system;
-pub mod wait_system;
-pub mod woodcutter_system;
-pub mod render_system;
 #[allow(clippy::module_inception)]
 pub mod game;
+pub mod navigation_system;
+pub mod render_system;
+pub mod utils;
+pub mod wait_system;
+pub mod woodcutter_system;
 
 pub use components::*;
-pub use utils::*;
+pub use game::*;
 pub use navigation_system::*;
+pub use render_system::*;
+pub use utils::*;
 pub use wait_system::*;
 pub use woodcutter_system::*;
-pub use render_system::*;
-pub use game::*;
